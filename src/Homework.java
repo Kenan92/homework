7882ea60a76c27d15f420fddc5ec6c8fb1d1c9ee
yhya <yhyasallom@gmail.com>
2019-04-24 01:24:12 +0300

    version2

--- src/Homework.java
+++ src/Homework.java
@@ -14,38 +14,31 @@ import java.io.IOException;
 
 
 public class Homework {
- public static BlockingQueue blockingQueue = new BlockingQueue(100);
- public static BlockingQueue blockingQueueNames = new BlockingQueue(100);
+
     public static void main(String[] args) throws InterruptedException, IOException {
-     JThread jf = new JThread();
-     jf.start();
-     jf.join();
+        BlockingQueue blockingQueueinfo = new BlockingQueue(100);
+ BlockingQueue blockingQueueNames = new BlockingQueue(100);
+     JThread jf = new JThread(blockingQueueNames);
+     Thread th1 =new Thread (jf);
+     th1.start();
    
-     ThReadQ [] thReadQs  = new ThReadQ[100];
-//     for (int i= 0 ; i<8 ; i++){
-//         if (i<7){
-//     thReadQs [i]= new ThReadQ(jf.get(),blockingQueue ,i*12,i*12+11);
-//         }else 
-//             
-//     thReadQs [i]= new ThReadQ(jf.get(),blockingQueue ,i*12,99);
-//     thReadQs[i].start();
-//     
-//     }
-     for (int i = 0 ; i<100 ; i++){
-     
-     thReadQs[i] =new ThReadQ(jf.get(), blockingQueue);
+     ThReadQ [] thReadQs  = new ThReadQ[8];
+     for (int i= 0 ; i<8 ; i++){
+        
+     thReadQs [i]= new ThReadQ(blockingQueueNames,blockingQueueinfo);
      thReadQs[i].start();
+        
      }
-     for (int i= 0 ; i<100 ; i++){
+   
+     
+     ThreadResultReadQ threadResultReadQ = new ThreadResultReadQ(blockingQueueinfo); 
+   
+     threadResultReadQ.start();
+     th1.join();
+     for (int i= 0 ; i<8 ; i++){
      thReadQs[i].join();
      }
-    
-      for (int i= 0 ; i<100 ; i++){
-     ThreadResultReadQ threadResultReadQ = new ThreadResultReadQ(blockingQueue);
-
-  
-    }
-// FileUtils.appendStringToFile("yhya.txt","yhya yy");
+     threadResultReadQ.join();
     }
     
 }
